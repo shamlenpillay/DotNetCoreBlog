1771c1e6e3713e1ac5c240aadf279295ce22cc18
Shamlen Pillay <shamlen.pillay@outlook.com>
2021-08-16 15:33:46 +0200

    Created Project and Initial Commit

--- DotNetCoreBlog/Startup.cs
+++ DotNetCoreBlog/Startup.cs
@@ -27,7 +27,7 @@ namespace DotNetCoreBlog
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World! \nWelcome to Shamlen Pillay's Blog");
             });
         }
     }
