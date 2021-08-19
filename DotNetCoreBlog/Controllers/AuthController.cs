f10ec3ed6c64d8dd68bb7640d167ed2ae6b98743
Shamlen Pillay <shamlen.pillay@outlook.com>
2021-08-19 15:26:58 +0200

    -Added functionality to Login and Logout

---
+++ DotNetCoreBlog/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using DotNetCoreBlog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVm)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginVm.UserName, loginVm.Password, false, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
